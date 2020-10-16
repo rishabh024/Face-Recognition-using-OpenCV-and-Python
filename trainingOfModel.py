75e349f433e6eeede685cd7fd05de8c13e0d49fd
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-10-16 15:26:55 +0530

    project path is added

--- trainingOfModel.py
+++ trainingOfModel.py
@@ -1,3 +1,8 @@
 import cv2,os
 import numpy as np
 from PIL import Image
+
+# PIL (python image library) is used here for image processing
+# projectPath variable is absolute path of the directory where this file resides
+projectPath = os.path.dirname(os.path.abspath(__file__))
+
