f5d38e4f871b2248eb788eaa46c6e1657ceb1678
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-10-16 13:35:02 +0530

    CascadeClassifier is included

--- generatingDataset.py
+++ generatingDataset.py
@@ -1,2 +1,6 @@
 import cv2
 import os
+
+#  projectPath variable is the absolute path of the directory where this file resides
+projectPath = os.path.dirname(os.path.abspath(__file__))
+faceClassifier = cv2.CascadeClassifier(projectPath+'/haarcascade_frontalface_default.xml')
