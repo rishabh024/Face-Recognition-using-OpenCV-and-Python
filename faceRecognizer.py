3b8177a9257ffb4cf3e7a679ae9e0c203c424592
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-10-16 16:35:37 +0530

    LBPH recognizer is used for face recognition

--- faceRecognizer.py
+++ faceRecognizer.py
@@ -3,7 +3,8 @@ import cv2, os
 #  projectPath variable is the absolute path of the directory where this file resides
 projectPath = os.path.dirname(os.path.abspath(__file__))
 
-
+# LBPH (Local Binary Pattern Histogram) algorithm is used to recognize both front and side face.
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 
 
