b74190188ff083ef200134339a72a79138d4094d
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-10-16 16:36:31 +0530

    haarcascade_frontalface classifier is added

--- faceRecognizer.py
+++ faceRecognizer.py
@@ -6,7 +6,9 @@ projectPath = os.path.dirname(os.path.abspath(__file__))
 # LBPH (Local Binary Pattern Histogram) algorithm is used to recognize both front and side face.
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-
+# now, trained model is used to recognise the face of person
+recognizer.read(projectPath + '/model.yml')
+faceClassifier = cv2.CascadeClassifier(projectPath + '/haarcascade_frontalface_default.xml')
 
 
 
