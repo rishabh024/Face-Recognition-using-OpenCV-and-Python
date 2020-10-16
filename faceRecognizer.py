c2a260677990a2f99fb306a8d20a828db5555fdb
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-10-16 16:34:47 +0530

    libraries are added for face recognition

---
+++ faceRecognizer.py
@@ -0,0 +1,26 @@
+import cv2, os
+
+#  projectPath variable is the absolute path of the directory where this file resides
+projectPath = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
