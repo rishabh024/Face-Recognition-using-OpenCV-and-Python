58e9bf25b83523059b1e58d284fc53890d25ba1a
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-10-16 15:27:41 +0530

    LBPHFaceRecognizer is used for face recognition

--- trainingOfModel.py
+++ trainingOfModel.py
@@ -6,3 +6,6 @@ from PIL import Image
 # projectPath variable is absolute path of the directory where this file resides
 projectPath = os.path.dirname(os.path.abspath(__file__))
 
+# LBPH (Local Binary Pattern Histogram) algorithm is used to recognize the faces
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+cascadePath = projectPath + '/haarcascade_frontalface_default.xml'
